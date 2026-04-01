13fb0358b486dcb4329ae6582828fe319896a6bd
JohannesL2 <johannes.lindgren2002@gmail.com>
2026-04-01 20:35:40 +0200

    Created folder structure and HomeScreen

---
+++ app/src/main/java/com/johannesl2/ecotrack/ui/screens/HomeScreen.kt
@@ -0,0 +1,39 @@
+package com.johannesl2.ecotrack.ui.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HomeScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFF7F9F7))
+            .padding(20.dp)
+            .verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "EcoTrack",
+            style = MaterialTheme.typography.headlineMedium,
+            fontWeight = FontWeight.Bold,
+            color = Color.Green
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+    }
+}
